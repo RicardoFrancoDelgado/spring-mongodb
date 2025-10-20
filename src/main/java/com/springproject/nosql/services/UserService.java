01e56dcc5a9b3943e3e746c8c22abc12597e7aef
RicardoFrancoDelgado <rf041555@gmail.com>
2025-10-19 21:35:17 -0300

    :goal_net: feat: get user with id and exception handlers methods

--- src/main/java/com/springproject/nosql/services/UserService.java
+++ src/main/java/com/springproject/nosql/services/UserService.java
@@ -1,7 +1,9 @@
 package com.springproject.nosql.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.springproject.nosql.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,9 @@ public class UserService {
   public List<User> findAll() {
     return repository.findAll();
   }
-  
+
+  public User findById(String id) {
+      Optional<User> obj = repository.findById(id);
+      return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+  }
 }
