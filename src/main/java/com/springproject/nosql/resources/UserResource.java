0f46826dabfde7f2286ce9e7276e53cfac784a27
RicardoFrancoDelgado <rf041555@gmail.com>
2025-10-23 17:49:01 -0300

    :sparkles: feat: add endpoint from posts reffs to user

--- src/main/java/com/springproject/nosql/resources/UserResource.java
+++ src/main/java/com/springproject/nosql/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.springproject.nosql.domain.Post;
 import com.springproject.nosql.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -53,5 +54,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
