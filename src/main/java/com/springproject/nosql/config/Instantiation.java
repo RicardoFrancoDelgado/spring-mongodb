12bb57dfb8021f45bd4fca921416e3c0f2d6ced8
RicardoFrancoDelgado <rf041555@gmail.com>
2025-10-23 13:45:45 -0300

    :sparkles: feat: data author projections with DTO

--- src/main/java/com/springproject/nosql/config/Instantiation.java
+++ src/main/java/com/springproject/nosql/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.springproject.nosql.config;
 
 import com.springproject.nosql.domain.Post;
 import com.springproject.nosql.domain.User;
+import com.springproject.nosql.dto.AuthorDTO;
 import com.springproject.nosql.repository.PostRepository;
 import com.springproject.nosql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,17 +26,17 @@ public class Instantiation implements CommandLineRunner {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-        userRepository.deleteAll(); // limpar a coleção no mango db
-        postRepository.deleteAll();
+        userRepository.deleteAll(); // limpar a coleção de users antes de instanciar no mongo db
+        postRepository.deleteAll(); // limpa a coleção de post antes de instanciar no mongodb
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
