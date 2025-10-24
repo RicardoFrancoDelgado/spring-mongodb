880fa152564fac61bcd69788936383f43b613485
RicardoFrancoDelgado <rf041555@gmail.com>
2025-10-24 15:34:53 -0300

    :sparkles: feat: add comments to posts

--- src/main/java/com/springproject/nosql/config/Instantiation.java
+++ src/main/java/com/springproject/nosql/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.springproject.nosql.config;
 import com.springproject.nosql.domain.Post;
 import com.springproject.nosql.domain.User;
 import com.springproject.nosql.dto.AuthorDTO;
+import com.springproject.nosql.dto.CommentDTO;
 import com.springproject.nosql.repository.PostRepository;
 import com.springproject.nosql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
