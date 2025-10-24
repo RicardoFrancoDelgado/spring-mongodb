deef0979212134949a5952221eadff22c7ed4c01
RicardoFrancoDelgado <rf041555@gmail.com>
2025-10-24 18:21:27 -0300

    :sparkles: feat: add simple query with query methods

--- src/main/java/com/springproject/nosql/services/PostService.java
+++ src/main/java/com/springproject/nosql/services/PostService.java
@@ -6,6 +6,7 @@ import com.springproject.nosql.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Post not found!"));
+    }
+
+    public List<Post> findByTitle(String text) {
+       return repository.findByTitleContainingIgnoreCase(text);
     }
 }
