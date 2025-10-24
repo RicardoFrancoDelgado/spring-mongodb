55ff19d272e100bb2dc5a756b748bc2ee35bcad3
RicardoFrancoDelgado <rf041555@gmail.com>
2025-10-24 15:09:55 -0300

    :sparkles: feat: getting posts by id

--- src/main/java/com/springproject/nosql/repository/PostRepository.java
+++ src/main/java/com/springproject/nosql/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.springproject.nosql.repository;
 
 import com.springproject.nosql.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
 }
---
+++ src/main/java/com/springproject/nosql/services/PostService.java
@@ -0,0 +1,21 @@
+package com.springproject.nosql.services;
+
+import com.springproject.nosql.domain.Post;
+import com.springproject.nosql.repository.PostRepository;
+import com.springproject.nosql.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Post not found!"));
+    }
+}
