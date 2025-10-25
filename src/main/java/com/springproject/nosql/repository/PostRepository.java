df261c9086524dfecd487f3fed467c9205276fe7
RicardoFrancoDelgado <rf041555@gmail.com>
2025-10-25 13:33:27 -0300

    :sparkles: feat: add simple query with @Query

--- src/main/java/com/springproject/nosql/repository/PostRepository.java
+++ src/main/java/com/springproject/nosql/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.springproject.nosql.repository;
 
 import com.springproject.nosql.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text); // a declaração faz com o que o spring monte a consulta para a aplicação
+
+    @Query("{ 'title': {$regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
